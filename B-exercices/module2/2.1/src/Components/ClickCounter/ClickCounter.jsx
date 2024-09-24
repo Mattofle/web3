1c3b0a09ed9a7712c9560e07a3314b7abcfaf7c9
Erismann <matteo.erismann@student.vinci.be>
2024-09-24 15:23:19 +0200

    new:ex2.1-stored

--- B-exercices/module2/2.1/src/Components/ClickCounter/ClickCounter.jsx
+++ B-exercices/module2/2.1/src/Components/ClickCounter/ClickCounter.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 
 const ClickCounter = ({title, message = "🔥", hoverMessage}) => {
     console.log("ClickCounter + title" + title)
-    const [count, setCount] = useState(0)
+    const storedCount = JSON.parse(localStorage.getItem("count"));
+    const [count, setCount] = useState(storedCount ?? 0)
     const [hover, setHover] = useState(false)
     return (
         <div>
             <h2>{title}</h2>
             <h3>{hover ? hoverMessage : null}</h3>
-            <button onClick={() => setCount((count) => count + 1)}
+            <button onClick={() => {
+                setCount(count + 1)
+                localStorage.setItem("count", JSON.stringify(count + 1))
+            } }
                 onMouseEnter={() => setHover(!hover)}
                 onMouseLeave={() => setHover(!hover)}>
             count is {count}
