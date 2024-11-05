7994c4ff562deb991ef1a136f21b76f1721f9d5c
Mattofle <matteo.erismann@student.vinci.be>
2024-11-05 15:08:39 +0100

    update

--- A-tutoriels/module6/module6/mongo.js
+++ A-tutoriels/module6/module6/mongo.js
@@ -7,7 +7,7 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2];
 
-const url = `mongodb+srv://Mattofle:qddRiaNhfOFkPc3a@mattoflecluster.h3uus.mongodb.net/noteApp?retryWrites=true&w=majority`;
+const url = `mongodb+srv://Mattofle:${password}@mattoflecluster.h3uus.mongodb.net/noteApp?retryWrites=true&w=majority`;
 
 mongoose.set("strictQuery", false);
 
